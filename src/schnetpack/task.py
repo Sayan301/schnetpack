f8e983e1fc10f1ec3df17010e0e6bed98d297a82
Sayan Dutta <114142473+Sayan301@users.noreply.github.com>
2024-01-19 17:55:25 +0530

    Update task.py

--- src/schnetpack/task.py
+++ src/schnetpack/task.py
@@ -1,6 +1,6 @@
 import warnings
 from typing import Optional, Dict, List, Type, Any
-
+import csv
 import pytorch_lightning as pl
 import torch
 from torch import nn as nn
@@ -87,7 +87,7 @@ class UnsupervisedModelOutput(ModelOutput):
         for metric in self.metrics[subset].values():
             metric(pred[self.name])
 
-class AtomisticPretrain(pl.LightningModule):
+class AtomisticDenoise(pl.LightningModule):
     """
     The basic learning task in SchNetPack, which ties model, loss and optimizer together.
 
@@ -97,6 +97,7 @@ class AtomisticPretrain(pl.LightningModule):
         self,
         model: AtomisticModel,
         outputs: List[ModelOutput],
+        logfile: str,
         optimizer_cls: Type[torch.optim.Optimizer] = torch.optim.Adam,
         optimizer_args: Optional[Dict[str, Any]] = None,
         scheduler_cls: Optional[Type] = None,
@@ -104,6 +105,7 @@ class AtomisticPretrain(pl.LightningModule):
         scheduler_monitor: Optional[str] = None,
         warmup_steps: int = 0,
         pretrain_labels: Optional[Dict[str, Any]] = None,
+        epoch: Optional[int] = None,
     ):
         """
         Args:
@@ -130,6 +132,232 @@ class AtomisticPretrain(pl.LightningModule):
         self.lr = optimizer_args["lr"]
         self.warmup_steps = warmup_steps
         self.save_hyperparameters()
+        self.epoch = 1
+        self.logfile = logfile
+
+    def setup(self, stage=None):
+        if stage == "fit":
+            self.model.initialize_transforms(self.trainer.datamodule)
+
+    def forward(self, inputs: Dict[str, torch.Tensor]):
+        results = self.model(inputs)
+        return results
+
+    def loss_fn(self, pred, batch):
+        loss = 0.0
+        for output in self.outputs:
+            loss += output.calculate_loss(pred, batch)
+        return loss
+
+    # def log_metrics(self, pred, targets, subset):
+    #     for output in self.outputs:
+    #         output.update_metrics(pred, targets, subset)
+    #         for metric_name, metric in output.metrics[subset].items():
+    #             self.log(
+    #                 f"{subset}_{output.name}_{metric_name}",
+    #                 metric,
+    #                 on_step=(subset == "train"),
+    #                 on_epoch=(subset != "train"),
+    #                 prog_bar=False,
+    #             )
+
+    def apply_constraints(self, pred, targets):
+        for output in self.outputs:
+            for constraint in output.constraints:
+                pred, targets = constraint(pred, targets, output)
+        return pred, targets
+
+
+    def training_step(self, batch, batch_idx):
+
+        # targets = {
+        #     output.target_property: batch[output.target_property]
+        #     for output in self.outputs
+        #     if not isinstance(output, UnsupervisedModelOutput)
+        # }
+        # try:
+        #     targets["considered_atoms"] = batch["considered_atoms"]
+        # except:
+        #     pass
+        targets ={'scalar_representation': batch['noise']}
+        pred = self.predict_without_postprocessing(batch)
+        pred['scalar_representation'] = pred['scalar_representation'].flatten()
+    
+        # print("Targets = ", targets)
+        # print("Predictions = ", pred)
+        # pred, targets = self.apply_constraints(pred, targets)
+
+        loss = self.loss_fn(pred, targets)
+        # print("Loss = ", loss)
+        self.log("train_loss", loss, on_step=True, on_epoch=False, prog_bar=False)
+        # self.log_metrics(pred, targets, "train")
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        torch.set_grad_enabled(self.grad_enabled)
+
+        # targets = {
+        #     output.target_property: batch[output.target_property]
+        #     for output in self.outputs
+        #     if not isinstance(output, UnsupervisedModelOutput)
+        # }
+        # try:
+        #     targets["considered_atoms"] = batch["considered_atoms"]
+        # except:
+        #     pass
+        targets ={'scalar_representation': batch['noise']}
+        
+        # print("Batch[noise]", batch['noise'].shape)
+        # print("Targets = ", targets['scalar_representation'].shape)
+        pred = self.predict_without_postprocessing(batch)
+        pred['scalar_representation'] = pred['scalar_representation'].flatten()
+        # print("Predictions = ", pred['scalar_representation'].shape)
+        # pred, targets = self.apply_constraints(pred, targets)
+
+        loss = self.loss_fn(pred, targets)
+        self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        # self.log_metrics(pred, targets, "val")
+
+        loss_value = "{:.6f}".format(loss.item())
+        with open(self.logfile, 'a') as logfile:
+            writer = csv.DictWriter(logfile, fieldnames = ['epoch', 'val_loss'])
+            writer.writeheader()
+            writer.writerows([{'epoch': self.epoch, 'val_loss': loss_value}])
+            self.epoch += 1
+
+        return {"val_loss": loss}
+
+    def test_step(self, batch, batch_idx):
+        torch.set_grad_enabled(self.grad_enabled)
+
+        # targets = {
+        #     output.target_property: batch[output.target_property]
+        #     for output in self.outputs
+        #     if not isinstance(output, UnsupervisedModelOutput)
+        # }
+        # try:
+        #     targets["considered_atoms"] = batch["considered_atoms"]
+        # except:
+        #     pass
+        targets ={'scalar_representation': batch['noise']}
+        pred = self.predict_without_postprocessing(batch)
+        pred['scalar_representation'] = pred['scalar_representation'].flatten()
+        
+        # pred, targets = self.apply_constraints(pred, targets)
+
+        loss = self.loss_fn(pred, targets)
+
+        self.log("test_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        # self.log_metrics(pred, targets, "test")
+        return {"test_loss": loss}
+
+    def predict_without_postprocessing(self, batch):
+        pp = self.model.do_postprocessing
+        self.model.do_postprocessing = False
+        pred = self(batch)
+        self.model.do_postprocessing = pp
+        return pred
+
+    def configure_optimizers(self):
+        optimizer = self.optimizer_cls(
+            params=self.parameters(), **self.optimizer_kwargs
+        )
+
+        if self.scheduler_cls:
+            schedulers = []
+            schedule = self.scheduler_cls(optimizer=optimizer, **self.scheduler_kwargs)
+            optimconf = {"scheduler": schedule, "name": "lr_schedule"}
+            if self.schedule_monitor:
+                optimconf["monitor"] = self.schedule_monitor
+            # incase model is validated before epoch end (not recommended use of val_check_interval)
+            if self.trainer.val_check_interval < 1.0:
+                warnings.warn(
+                    "Learning rate is scheduled after epoch end. To enable scheduling before epoch end, "
+                    "please specify val_check_interval by the number of training epochs after which the "
+                    "model is validated."
+                )
+            # incase model is validated before epoch end (recommended use of val_check_interval)
+            if self.trainer.val_check_interval > 1.0:
+                optimconf["interval"] = "step"
+                optimconf["frequency"] = self.trainer.val_check_interval
+            schedulers.append(optimconf)
+            return [optimizer], schedulers
+        else:
+            return optimizer
+
+    def optimizer_step(
+        self,
+        epoch: int = None,
+        batch_idx: int = None,
+        optimizer=None,
+        optimizer_closure=None,
+    ):
+        if self.global_step < self.warmup_steps:
+            lr_scale = min(1.0, float(self.trainer.global_step + 1) / self.warmup_steps)
+            for pg in optimizer.param_groups:
+                pg["lr"] = lr_scale * self.lr
+
+        # update params
+        optimizer.step(closure=optimizer_closure)
+
+    def save_model(self, path: str, do_postprocessing: Optional[bool] = None):
+        if self.global_rank == 0:
+            pp_status = self.model.do_postprocessing
+            if do_postprocessing is not None:
+                self.model.do_postprocessing = do_postprocessing
+
+            torch.save(self.model, path)
+
+            self.model.do_postprocessing = pp_status
+
+
+class AtomisticPretrain(pl.LightningModule):
+    """
+    The basic learning task in SchNetPack, which ties model, loss and optimizer together.
+
+    """
+
+    def __init__(
+        self,
+        model: AtomisticModel,
+        outputs: List[ModelOutput],
+        logfile: str,
+        optimizer_cls: Type[torch.optim.Optimizer] = torch.optim.Adam,
+        optimizer_args: Optional[Dict[str, Any]] = None,
+        scheduler_cls: Optional[Type] = None,
+        scheduler_args: Optional[Dict[str, Any]] = None,
+        scheduler_monitor: Optional[str] = None,
+        warmup_steps: int = 0,
+        pretrain_labels: Optional[Dict[str, Any]] = None,
+        epoch: Optional[int] = None,
+    ):
+        """
+        Args:
+            model: the neural network model
+            outputs: list of outputs an optional loss functions
+            optimizer_cls: type of torch optimizer,e.g. torch.optim.Adam
+            optimizer_args: dict of optimizer keyword arguments
+            scheduler_cls: type of torch learning rate scheduler
+            scheduler_args: dict of scheduler keyword arguments
+            scheduler_monitor: name of metric to be observed for ReduceLROnPlateau
+            warmup_steps: number of steps used to increase the learning rate from zero
+              linearly to the target learning rate at the beginning of training
+        """
+        super().__init__()
+        self.model = model
+        self.optimizer_cls = optimizer_cls
+        self.optimizer_kwargs = optimizer_args
+        self.scheduler_cls = scheduler_cls
+        self.scheduler_kwargs = scheduler_args
+        self.schedule_monitor = scheduler_monitor
+        self.outputs = nn.ModuleList(outputs)
+        self.pretrain_labels = pretrain_labels
+        self.grad_enabled = len(self.model.required_derivatives) > 0
+        self.lr = optimizer_args["lr"]
+        self.warmup_steps = warmup_steps
+        self.save_hyperparameters()
+        self.logfile = logfile
+        self.epoch = 1
 
     def setup(self, stage=None):
         if stage == "fit":
@@ -210,7 +438,12 @@ class AtomisticPretrain(pl.LightningModule):
         loss = self.loss_fn(pred, targets)
         self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
         # self.log_metrics(pred, targets, "val")
-
+        loss_value = "{:.6f}".format(loss.item())
+        with open(self.logfile, 'a') as logfile:
+            writer = csv.DictWriter(logfile, fieldnames = ['epoch', 'val_loss'])
+            writer.writeheader()
+            writer.writerows([{'epoch': self.epoch, 'val_loss': loss_value}])
+            self.epoch += 1
         return {"val_loss": loss}
 
     def test_step(self, batch, batch_idx):
@@ -305,12 +538,14 @@ class AtomisticTask(pl.LightningModule):
         self,
         model: AtomisticModel,
         outputs: List[ModelOutput],
+        logfile: str,
         optimizer_cls: Type[torch.optim.Optimizer] = torch.optim.Adam,
         optimizer_args: Optional[Dict[str, Any]] = None,
         scheduler_cls: Optional[Type] = None,
         scheduler_args: Optional[Dict[str, Any]] = None,
         scheduler_monitor: Optional[str] = None,
         warmup_steps: int = 0,
+        epoch: Optional[int] = None,
     ):
         """
         Args:
@@ -337,6 +572,8 @@ class AtomisticTask(pl.LightningModule):
         self.lr = optimizer_args["lr"]
         self.warmup_steps = warmup_steps
         self.save_hyperparameters()
+        self.logfile = logfile
+        self.epoch = 1
 
     def setup(self, stage=None):
         if stage == "fit":
@@ -387,7 +624,6 @@ class AtomisticTask(pl.LightningModule):
 
         self.log("train_loss", loss, on_step=True, on_epoch=False, prog_bar=False)
         self.log_metrics(pred, targets, "train")
-        print("Loss type = ", type(loss))
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -410,6 +646,13 @@ class AtomisticTask(pl.LightningModule):
 
         self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log_metrics(pred, targets, "val")
+        
+        loss_value = "{:.6f}".format(loss.item())
+        with open(self.logfile, 'a') as logfile:
+            writer = csv.DictWriter(logfile, fieldnames = ['epoch', 'val_loss'])
+            writer.writeheader()
+            writer.writerows([{'epoch': self.epoch, 'val_loss': loss_value}])
+            self.epoch += 1
 
         return {"val_loss": loss}
 
